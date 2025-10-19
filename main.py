283883f2a8d0b87dcc4d6960dc9f2160d5e3c61d
Brijes Mandal <brijes.mandal01@gmail.com>
2025-10-19 01:04:42 -0500

    Security Camera Logic Added

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ cam = cv2.VideoCapture(0)  # given access to default camera
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")  # detects faces
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")  # detects body
 
-detection = True # face or body detection object
+detection = False # face or body detection object
+timer_started = False 
+seconds_limit = 5 # if face or body not in 5 secs in frame, stop the video
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")    #compression method
 frame_size = (int(cam.get(3)),int(cam.get(4))) #get heigth and width from the camera object of VideoCapture Class
-out = cv2.VideoWriter("video.mp4", fourcc, 20, frame_size)
+
+# counting the number of videos recorder
+count = 0
 
 while True:
     _, frame = cam.read()  # read each frame 
@@ -24,16 +28,43 @@ while True:
     # for (x,y,width,height) in faces:
     #     cv2.rectangle(frame, (x,y), (x+width,y+height), (255,0,0), 3)
 
+    # complex logic for security camera 😂
+
     if (len(faces) + len(bodies) > 0):  # if either face or body detected
-        detection = True
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            filename = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            # create a video writer objects that writes frame
+            out = cv2.VideoWriter(f"{filename}.mp4", fourcc, 20, frame_size)  
+            count = count + 1
+            print(("Started Recording!"))
+    elif detection:
+        if timer_started:
+            if time.time() - current_time >= 5:
+                detection = False
+                timer_started = False
+                out.release()
+                print("Stopped Recording!")
+        else:
+            timer_started = True
+            current_time = time.time()
     
-    out.write(frame) # write the frame in video
+
+    if detection:
+        out.write(frame) # write the frame in video
 
     cv2.imshow("Camera", frame)  # show the frame read
 
     if cv2.waitKey(1) == ord('q'):   # code to break from the loop 
+        if detection:
+            out.release()
+            print("Stopped Recording due to administrator input.")
         break
 
-out.release()
 cam.release()   # released device camera from the object
 cv2.destroyAllWindows()   
+
+print("Program Ended.")
+print(f"Total of {count} videos recorded.")
