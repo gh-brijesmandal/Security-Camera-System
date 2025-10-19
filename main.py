fbf97c605f15de2dc73133cc967841545ec4c83a
Brijes Mandal <brijes.mandal01@gmail.com>
2025-10-18 23:22:04 -0500

    Rectangles on faces

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ import datetime
 # creating camera object
 cam = cv2.VideoCapture(0)  # given access to default camera
 
+# importing the cascade classifiers
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")  # detects faces
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")  # detects body
+
 while True:
     _, frame = cam.read()  # read each frame 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # changes frame to gray color
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # detects face and sends x,y,width,height of faces in list
+
+    for (x,y,width,height) in faces:
+        cv2.rectangle(frame, (x,y), (x+width,y+height), (255,0,0), 3)
 
     cv2.imshow("Camera", frame)  # show the frame read
 
